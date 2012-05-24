d95828cc879369a2978fb05de6f030f08c972739
Kate Hudson <katehudsondesign@gmail.com>
2012-05-24 10:12:00 -0700

    added files

---
+++ js/script.js
@@ -0,0 +1,199 @@
+var featuredData = { 
+		featured: [
+			{
+				index: 0,
+				videoTitle: "Casa Blanca",
+				videoTime: "1:20:20",
+				videoDesc: "The minified versions, while having a larger file size than the packed versions (note: packed version is not available in current release), are generally the best versions to use on production deployments. The packed versions require non-trivial client-side processing time to uncompress (unpack) the code whereas the minified versions do not. The packed versions of jQuery will take less time to download than the minified or uncompressed versions; however, each time the library is loaded (initially or from the browser cache) it will need to be uncompressed which will cause a non-trivial delay in the execution of any jQuery code each time it is loaded. ",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-01",
+				fileTypes: [ { type: "mp4", src:"trailer.mp4" }, { type: "ogv", src:"trailer.ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 1,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 2,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 3,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 4,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 5,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 6,
+				videoTitle: "Casa Blanca",
+				videoTime: "1:20:20",
+				videoDesc: "The minified versions, while having a larger file size than the packed versions (note: packed version is not available in current release), are generally the best versions to use on production deployments. The packed versions require non-trivial client-side processing time to uncompress (unpack) the code whereas the minified versions do not. The packed versions of jQuery will take less time to download than the minified or uncompressed versions; however, each time the library is loaded (initially or from the browser cache) it will need to be uncompressed which will cause a non-trivial delay in the execution of any jQuery code each time it is loaded. ",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-01",
+				fileTypes: [ { type: "mp4", src:"trailer.mp4" }, { type: "ogv", src:"trailer.ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 7,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 8,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			},
+			{
+				index: 9,
+				videoTitle: "DDASDASss",
+				videoTime: 343,
+				videoDesc: "A movie about stuff",
+				videoPreview: "oceans-clip.ogv",
+				videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+				dateUploaded: "2012-01-14",
+				fileTypes: [ { type: "mp4" }, { type: "ogv" } ],
+				tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+			}
+		],
+		previewPopcorn: function() {
+			//Do popcorn stuff
+		}
+};
+
+var detailsData = {
+	index: 0,
+	videoTitle: "Casa Blanca",
+	videoTime: "1:20:20",
+	videoDesc: "The minified versions, while having a larger file size than the packed versions (note: packed version is not available in current release), are generally the best versions to use on production deployments. The packed versions require non-trivial client-side processing time to uncompress (unpack) the code whereas the minified versions do not. The packed versions of jQuery will take less time to download than the minified or uncompressed versions; however, each time the library is loaded (initially or from the browser cache) it will need to be uncompressed which will cause a non-trivial delay in the execution of any jQuery code each time it is loaded. ",
+	videoPreview: "oceans-clip.ogv",
+	videoPoster: "img/10-Bhante-Mangala-First-Talk-May-2009-640.gif",
+	dateUploaded: "2012-01-01",
+	fileTypes: [ { type: "mp4", src:"trailer.mp4" }, { type: "webm", src:"trailer.webm" }, { type: "ogv", src:"trailer.ogv" } ],
+	tags: [ { tagName: "silly" }, { tagName: "cool" } ]
+};
+
+var tagsData = {
+	tags: [
+		{
+			name: "happy",
+			url: "#happy"
+		},
+		{
+			name: "sad",
+			url: "#sad"
+		},
+		{
+			name: "news",
+			url: "#news"
+		},
+]};
+
+//TABS
+$(".widget-nav li a ").click(function(){
+	var activeTabID = $(this).attr("href"),
+		tabContainerID = "#tabs",
+		$activeTab = $(activeTabID),
+		$tabContainer = $(tabContainerID);
+
+	$(".active").removeClass("active");
+	$(this).addClass("active");
+
+	$activeTab.addClass("active");
+});
+
+$("#search-field").change(function(){
+	$(".active").removeClass("active");
+	$("#search-results").addClass("active");
+	$(".widget-search").addClass("active");
+});
+
+//TEMPLATE PARTS
+mainTemplate( featuredData );
+createTemplate("tags-template", tagsData, "tags");
+function mainTemplate( data ) {
+	var template = document.getElementById("featured-template").innerHTML.replace(/%7B%7B/g, '{{').replace(/%7D%7D/g, '}}'),
+		html = Mustache.render(template, data);
+	document.getElementById("featured").innerHTML = html;
+
+	//Show details
+	$(".show-details").click(function(e){
+		e.preventDefault();
+		var videoID = $(this).attr("href"),
+			videoData;
+		//Real application uses ajax to retreive the data for that video.
+		videoData = detailsData;
+		createTemplate( "details-template", videoData, "details" );
+		$( "#featured" ).removeClass("active");
+		$( "#details" ).addClass("active");	
+
+		//Close me button
+		$( "#details .close-me" ).click(function(e){
+			e.preventDefault();
+			$( "#details" ).removeClass("active");
+			$( "#featured" ).addClass("active");
+		});
+	});
+}
+
+function createTemplate( templateID, data, targetID ) {
+	var template = document.getElementById(templateID).innerHTML.replace(/%7B%7B/g, '{{').replace(/%7D%7D/g, '}}'),
+		html = Mustache.render(template, data);
+	document.getElementById(targetID).innerHTML = html;
+}
+
